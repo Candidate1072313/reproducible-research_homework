a035ef6856c17002019b499e8d1f356fcc200d6c
Candidate 1072313 <jessie.durso@keble.ox.ac.uk>
2024-12-08 16:18:01 +0000

    Brownian Motion Stimulation

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -1,55 +1,42 @@
-#install.packages("ggplot2")
-#install.packages("gridExtra")
-
+install.packages("ggplot2")
+install.packages("gridExtra")
 library(ggplot2)
 library(gridExtra)
 
+#Set seed for reproducibility
+set.seed(123)
+
 random_walk  <- function (n_steps) {
-  
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
-  
   df[1,] <- c(0,0,1)
-  
   for (i in 2:n_steps) {
-    
     h <- 0.25
-    
     angle <- runif(1, min = 0, max = 2*pi)
-    
     df[i,1] <- df[i-1,1] + cos(angle)*h
-    
     df[i,2] <- df[i-1,2] + sin(angle)*h
-    
     df[i,3] <- i
-    
   }
-  
   return(df)
-  
 }
 
 data1 <- random_walk(500)
-
 plot1 <- ggplot(aes(x = x, y = y), data = data1) +
-  
   geom_path(aes(colour = time)) +
-  
   theme_bw() +
-  
   xlab("x-coordinate") +
-  
   ylab("y-coordinate")
 
+#Generating second graph from data2
 data2 <- random_walk(500)
-
 plot2 <- ggplot(aes(x = x, y = y), data = data2) +
-  
   geom_path(aes(colour = time)) +
-  
   theme_bw() +
-  
   xlab("x-coordinate") +
-  
   ylab("y-coordinate")
 
 grid.arrange(plot1, plot2, ncol=2)
+
+#Recording packages used
+sink(file = "package-versions-brownian.txt")
+sessionInfo()
+sink()
